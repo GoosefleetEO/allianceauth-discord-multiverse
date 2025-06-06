113177cb90667e584fa9554791efc2597fcea42f
digital-pet <47679051+digital-pet@users.noreply.github.com>
2025-06-06 16:23:43 -0400

    Increment version

--- aadiscordmultiverse/__init__.py
+++ aadiscordmultiverse/__init__.py
@@ -1,6 +1,6 @@
 """
 Service module for managing an unlimited number of discord servers from a single auth instance. It can run side by side with the core auth discord service, or completely standalone.
 """
-__version__ = "0.0.1a7"
+__version__ = "0.0.1a8"
 __branch__ = "Gooseflock"
 __title__ = "Discord Multiverse"
